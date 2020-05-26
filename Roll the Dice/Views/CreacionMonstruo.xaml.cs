b79b15610af0e242a2ea299703c0cfc0e6f15533
iZaWar1 <victorpelotudo@gmail.com>
2020-05-26 17:22:02 +0200

    Creaciones de Creaciones

---
+++ Roll the Dice/Views/CreacionMonstruo.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Roll_the_Dice.Views
+{
+    /// <summary>
+    /// Lógica de interacción para CreacionMonstruo.xaml
+    /// </summary>
+    public partial class CreacionMonstruo : Window
+    {
+        public CreacionMonstruo()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
