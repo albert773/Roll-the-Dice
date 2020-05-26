cccf57788218af2deb4d57e2d5ad7030a27269bf
Albert <murderer773@gmail.com>
2020-05-26 17:39:15 +0200

    Menus a Eliminar

---
+++ Roll the Dice/Views/MenuUsuario.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Roll_the_Dice.Views
+{
+    /// <summary>
+    /// Lógica de interacción para MenuUsuario.xaml
+    /// </summary>
+    public partial class MenuUsuario : Window
+    {
+
+        bool MenuClosed = false;
+        public MenuUsuario()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (MenuClosed)
+            {
+                Storyboard openMenu = (Storyboard)button.FindResource("MenuOpen");
+                openMenu.Begin();
+            }
+            else {
+                Storyboard closeMenu = (Storyboard)button.FindResource("MenuClose");
+                closeMenu.Begin();
+            }
+        }
+    }
+}
