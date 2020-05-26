de45bdc9981d355feaf85d98cc38374bff73ea04
jmhs11 <josemariahs@gmail.com>
2020-05-26 17:25:07 +0200

    Insertar web service

---
+++ Roll the Dice Service/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Threading;
+using Roll_the_Dice_Service.Models;
+
+namespace Roll_the_Dice_Service.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/login")]
+    public class LoginController : ApiController
+    {
+        [HttpPost]
+        [Route("authenticate")]
+        public IHttpActionResult Authenticate(LoginRequest login)
+        {
+            if (login == null)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            //TODO: Validate credentials Correctly, this code is only for demo !!
+            bool isCredentialValid = (login.Password == "123456");
+            if (isCredentialValid)
+            {
+                var token = TokenGenerator.GenerateTokenJwt(login.Email);
+                return Ok(token);
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ Roll the Dice Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Roll_the_Dice_Service.WebApiApplication" Language="C#" %>
