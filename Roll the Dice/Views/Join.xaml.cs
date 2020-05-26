3da854af9701e24dcd34ede682ab2aa53566a6e2
Albert <murderer773@gmail.com>
2020-05-26 17:39:00 +0200

    Unir views creacion de botones y creacionde menus a eliminar

--- Roll the Dice/Views/Join.xaml.cs
+++ Roll the Dice/Views/Join.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Roll_the_Dice.Views
 {
@@ -22,6 +10,13 @@ namespace Roll_the_Dice.Views
         public Join()
         {
             InitializeComponent();
+        }
+
+        private void CreateJoin_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new CreateJoin(); //create your new form.
+            newForm.Show(); //show the new form.
+            this.Close(); //only if you want to close the current form.
         }
     }
 }
