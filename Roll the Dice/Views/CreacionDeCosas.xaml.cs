107e0d0ca5e045e1d5cdcf7dfbf73946d5dfd9b7
iZaWar1 <victorpelotudo@gmail.com>
2020-05-29 17:24:24 +0200

    Fusion de los dos anteriores

--- Roll the Dice/Views/CreacionDeCosas.xaml.cs
+++ Roll the Dice/Views/CreacionDeCosas.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Roll_the_Dice.Views
@@ -22,7 +23,27 @@ namespace Roll_the_Dice.Views
         public CreacionDeCosas()
         {
             InitializeComponent();
+            creationFrame.NavigationUIVisibility = NavigationUIVisibility.Hidden;
+        }
+
+        private void arma_Checked(object sender, RoutedEventArgs e)
+        {
             creationFrame.Content = new CreacionArma();
+        }
+
+        private void armadura_Checked(object sender, RoutedEventArgs e)
+        {
+            creationFrame.Content = new CreacionArmaduras();
+        }
+
+        private void npc_Checked(object sender, RoutedEventArgs e)
+        {
+            creationFrame.Content = new CreacionNPC();
+        }
+
+        private void monstruo_Checked(object sender, RoutedEventArgs e)
+        {
+            creationFrame.Content = new CreacionMonstruo();
         }
     }
 }
--- Roll the Dice/Views/EliminarCosas.xaml.cs
+++ Roll the Dice/Views/EliminarCosas.xaml.cs
@@ -20,7 +20,9 @@ namespace Roll_the_Dice.Views
     /// </summary>
     public partial class EliminarCosas : Window
     {
-        
+
+        List<String> itemList = new List<String>() { "Arma1", "Arma2", "Arma3", "Arma4", "Arma5", "Arma6","Arma7", "Arma8", "Arma9", "Arma10",
+            "Arma11", "Arma12", "Arma13", "Arma14", "Arma15", "Arma16","Arma17", "Arma18", "Arma19", "Arma20",};
 
         public EliminarCosas()
         {
@@ -32,13 +34,21 @@ namespace Roll_the_Dice.Views
         {
             //Acabar de mirar
             //var itemList = MenuGM.armas.ToList();
-            List<String> itemList = new List<String>() { "Arma1", "Arma2", "Arma3", "Arma4", "Arma5", "Arma6","Arma7", "Arma8", "Arma9", "Arma10",
-            "Arma11", "Arma12", "Arma13", "Arma14", "Arma15", "Arma16","Arma17", "Arma18", "Arma19", "Arma20",};
-            foreach (var item in itemList) {
+            foreach (var item in itemList)
+            {
                 //CheckBox chk = new CheckBox();
                 //chk.Content = item.ToString();
                 //st1.Items.Add(chk);
+
                 st1.Items.Add(item);
+            }
+        }
+
+        public void EliminarItems_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var item in itemList)
+            {
+
             }
         }
     }
