f16d069a64e3463298f04d52db2543898a6e0e0b
iZaWar1 <victorpelotudo@gmail.com>
2020-05-29 12:42:26 +0200

    Beauty changes

--- Roll the Dice/Views/MenuGM.xaml.cs
+++ Roll the Dice/Views/MenuGM.xaml.cs
@@ -32,6 +32,7 @@ namespace Roll_the_Dice.Views
         {
             InitializeComponent();
             //reloadListCrear();
+            frameMap.Content = new Mapa();
         }
         private void Create_Click(object sender, RoutedEventArgs e)
         {
