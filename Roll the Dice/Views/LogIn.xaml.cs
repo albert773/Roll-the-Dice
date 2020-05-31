e0326306f1a873df661708c947120763d08113dc
Albert <murderer773@gmail.com>
2020-05-31 18:07:29 +0200

    Incorporacion de advertencias en login y registrarse

--- Roll the Dice/Views/LogIn.xaml.cs
+++ Roll the Dice/Views/LogIn.xaml.cs
@@ -9,6 +9,7 @@ namespace Roll_the_Dice.Views
     /// </summary>
     public partial class LogIn : Page
     {
+        Boolean confirm = true;
         public LogIn()
         {
             InitializeComponent();
@@ -16,6 +17,21 @@ namespace Roll_the_Dice.Views
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            errorMail.Visibility = Visibility.Hidden;
+            errorPass.Visibility = Visibility.Hidden;
+            borderMessageCorreo.Visibility = Visibility.Hidden;
+            borderMessagePass.Visibility = Visibility.Hidden;
+            correoMessageError.Visibility = Visibility.Hidden;
+            textErrorPass.Visibility = Visibility.Hidden;
+
+
+            /*if (correoText.Text.Equals(""))
+            {
+                confirm = false;
+                errorMail.Visibility = Visibility.Visible;
+            }*/
+            //if (true) confirm = false;
+            //if(confirm)
             NavigationService.Navigate(new CreateJoin());
         }
 
