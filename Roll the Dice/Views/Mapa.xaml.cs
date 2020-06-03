f3912d6e54ef410d0e50d8c7d88ac4b5ac93de1f
iZaWar1 <victorpelotudo@gmail.com>
2020-06-03 20:07:17 +0200

    Mapa bonico

--- Roll the Dice/Views/Mapa.xaml.cs
+++ Roll the Dice/Views/Mapa.xaml.cs
@@ -20,9 +20,71 @@ namespace Roll_the_Dice.Views
     /// </summary>
     public partial class Mapa : Page
     {
+        List<ColumnDefinition> colList = new List<ColumnDefinition>();
+        List<RowDefinition> rowList = new List<RowDefinition>();
+        List<Button> buttonList = new List<Button>();
+        List<Rectangle> rectangleList = new List<Rectangle>();
+        const int X = 15;
         public Mapa()
         {
             InitializeComponent();
+           
+            for(int i=0; i< X; i++)
+            {
+                colList.Add(new ColumnDefinition());
+                rowList.Add(new RowDefinition());
+            }
+            for (int i=0; i<colList.Count(); i++)
+            {
+                mapa.ColumnDefinitions.Add(colList[i]);
+                mapa.RowDefinitions.Add(rowList[i]);
+            }
+            for (int x = 0; x < X; x++)
+            {
+                for (int y = 0; y < X; y++)
+                {
+                    Button but = new Button();
+                    but.Name = "b"+x+"b"+y;
+                    but.Background = null;
+                    but.Height = Double.NaN;
+                    but.Width = Double.NaN;
+                    but.Click += textSwap;
+                    buttonList.Add(but);
+                    Rectangle rec = new Rectangle();
+                    rec.Name = "r" + x + "r" + y;
+                    rec.Opacity = 0.3;
+                    rec.Fill = Brushes.Red;
+                    rec.Height = Double.NaN;
+                    rec.Width = Double.NaN;
+                    rectangleList.Add(rec);
+                }
+            }
+            int buttonPos = 0;
+            for(int x=0; x<X; x++)
+            {
+                for(int y=0; y<X; y++)
+                {
+
+                        Grid.SetColumn(rectangleList[buttonPos], x);
+                        Grid.SetRow(rectangleList[buttonPos], y);
+                        Grid.SetColumn(buttonList[buttonPos], x);
+                        Grid.SetRow(buttonList[buttonPos], y);
+                        buttonPos++;
+
+                    
+                }
+            }
+            for(int i=0; i<(X*X); i++)
+            {
+                mapa.Children.Add(rectangleList[i]);
+                mapa.Children.Add(buttonList[i]);
+            }
+
+        }
+        public void textSwap(object sender, RoutedEventArgs e)
+        {
+            var but = (Button)sender;
+            but.Content = but.Name;
         }
     }
 }
