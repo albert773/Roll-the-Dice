987dfef362f8ec96dfeea826e258ae9ef7b21cf1
jmhs11 <josemariahs@gmail.com>
2020-06-05 23:33:35 +0200

    Actualizacion Web Service: Modificacion Singleton

--- RolltheDiceService/Controllers/SingletonController.cs
+++ RolltheDiceService/Controllers/SingletonController.cs
@@ -19,15 +19,31 @@ namespace RolltheDiceService.Controllers
             return Ok(s);
         }
 
+        [HttpGet]
+        [Route("dados")]
+        public IHttpActionResult GetDados()
+        {
+            return Ok(Singleton.Dado);
+        }
+
         [HttpPost]
         [Route("")]
         public IHttpActionResult Post(string[] s)
         {
             Singleton.Accion = s[0];
-            Singleton.Emisor = Int32.Parse(s[1]);
-            Singleton.Receptor = Int32.Parse(s[2]);
-            object[] s1 = { Singleton.Accion, Singleton.Emisor, Singleton.Receptor };
+            Singleton.Elemento = Int32.Parse(s[1]);
+            Singleton.Emisor = Int32.Parse(s[2]);
+            Singleton.Receptor = Int32.Parse(s[3]);
+            object[] s1 = { Singleton.Accion, Singleton.Elemento, Singleton.Emisor, Singleton.Receptor };
             return Ok(s1);
+        }
+
+        [HttpPost]
+        [Route("dados")]
+        public IHttpActionResult Post(int d)
+        {
+            Singleton.Dado = d;
+            return Ok();
         }
     }
 }
